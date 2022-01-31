6ff57e5dc1f3e5308aad388294976e6343fde022
netzgestaltung <t.fellinger@netzgestaltung.at>
2022-01-31 22:13:22 +0100

    Update config.php

--- mu-plugins/stats-matomo/config.php
+++ mu-plugins/stats-matomo/config.php
@@ -11,13 +11,13 @@
  */
 
 /** Matomo base URL, for example http://example.org/matomo/ Must be set */
-define( 'SOCOS_IO_STATS_TRACKER_URL', '' );
+define( 'STATS_MATOMO_STATS_TRACKER_URL', '' );
 
 /** Specify the site ID to track */
-define( 'SOCOS_IO_STATS_SITE_ID', 1 );
+define( 'STATS_MATOMO_STATS_SITE_ID', 1 );
 
 /**
  * Specify an API token with at least Write permission, so the Visitor IP address can be recorded
  * Learn more about token_auth: https://matomo.org/faq/general/faq_114/
  */
-define( 'SOCOS_IO_STATS_AUTH_TOKEN', '' );
+define( 'STATS_MATOMO_STATS_AUTH_TOKEN', '' );
